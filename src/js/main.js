e35caaf807616e5575c53f4673ce7d6ba0db186b
karachevtsev <karachevtsev@yahoo.com>
2016-06-10 01:28:05 +0300

    Add contacts section

--- src/js/main.js
+++ src/js/main.js
@@ -47,4 +47,22 @@ $(document).ready(function(){
         });
     }, {offset:'80%'});
 
+    $('#contact-form .form-control').each(function(){
+
+        if($.trim($(this).val()) === ''){
+            $(this).removeClass('input-filled');
+        }else{
+            $(this).addClass('input-filled');
+        }
+    });
+
+    $('#contact-form .form-control').on('blur',function(){
+
+        if($.trim($(this).val()) === ''){
+            $(this).removeClass('input-filled');
+        }else{
+            $(this).addClass('input-filled');
+        }
+    });
+
 });
